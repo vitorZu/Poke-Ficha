bb5ff88dfafa03a7f2f205bfd816e6e1abe495a3
Vitor Augusto Zucon Gonçalves <vazgoncalves@rede.zanardo.com.br>
2023-01-18 11:23:14 -0300

    gerando PDF (sem imagens)

--- pokeficha/index/views.py
+++ pokeficha/index/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 import urllib.request
 import json
 import io
-from docx import Document
-from docx.shared import Inches
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
 import requests
+
+# from docx import Document
+# from docx.shared import Inches
 
 # Create your views here.
 def index(request):
@@ -30,18 +35,56 @@ def index(request):
             "foto-shiny": str(list_of_data['sprites']['other']['official-artwork']['front_shiny'])
         }
 
-        response = requests.get(data['foto'], stream=True)
-        foto = io.BytesIO(response.content)
-        response = requests.get(data['foto-shiny'], stream=True)
-        foto_shiny = io.BytesIO(response.content)
+        # criando BIOstream and fotos
+        buf = io.BytesIO()
+        response1 = requests.get(data['foto'], stream=True)
+        foto = io.BytesIO(response1.content)
+        response2 = requests.get(data['foto-shiny'], stream=True)
+        foto_shiny = io.BytesIO(response2.content)
 
-        document = Document()
-        document.add_heading(f"{data['nome']} - {data['id']}", 0)
-        document.add_picture(foto, width=Inches(1.25))
-        document.add_picture(foto_shiny, width=Inches(1.25))
+        # Criando Canvas
+        c = canvas.Canvas(buf, pagesize = letter, bottomup = 0)
+
+        # Configurando Pagina PDF
+        textob = c.beginText()
+        textob.setTextOrigin(inch, inch)
+        textob.setFont("Helvetica", 14)
+
+        #linha do Texto
+        lines = [
+            data['id'],
+            data['nome'],
+            data['altura'] + " M",
+            data['peso'] + " kg",
+            f"{foto}",
+            f"{foto_shiny}",
+            "=============",
+        ]
+
+        #looping
+        for line in lines:
+            textob.textLine(line)
+
+
+        #finalizando
+        c.drawText(textob)
+        c.showPage()
+        c.save()
+        buf.seek(0)
+
+        #return
+        return FileResponse(buf, as_attachment = True, filename = 'orçamento.pdf')
+
+
         
 
-        document.save('../docx/demo.docx')
+        # document = Document()
+        # document.add_heading(f"{data['nome']} - {data['id']}", 0)
+        # document.add_picture(foto, width=Inches(1.25))
+        # document.add_picture(foto_shiny, width=Inches(1.25))
+        
+
+        # document.save('demo.docx')
 
     else:
         data = {}
